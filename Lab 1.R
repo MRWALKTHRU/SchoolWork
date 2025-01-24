59228f0405ab26aa802bc8035f2d4874a79d2c63
Jules Gravestock <114362927+MRWALKTHRU@users.noreply.github.com>
2025-01-24 16:41:02 -0500

    Add files via upload

---
+++ Lab 1.R
@@ -0,0 +1,16 @@
+#Practice Activities
+Age <- c(18,19,20)
+Dukes <- c("Bo","Luke","Daisy")
+Cousin <- data.frame(Dukes,Age)
+anorexia[1]
+
+# Question 3 of Lab Assignment
+Day <- c("Monday","Tuesday","Wednesday","Thursday","Friday")
+Date <- c(22,23,24,25,26)
+Calendar <- data.frame(Day,Date)
+
+Calendar[2, 1]
+Calendar[4,2]
+Calendar[3,]
+Calendar[1]
+
