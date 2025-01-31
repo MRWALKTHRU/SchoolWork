b99cdc91c1a98524be1cafd0f09de96179e4a72e
Jules Gravestock <114362927+MRWALKTHRU@users.noreply.github.com>
2025-01-31 13:03:30 -0500

    Add files via upload

---
+++ PSYCH292/Lab3.R
@@ -0,0 +1,17 @@
+Eggs <- c( 80, 70, 60, 60, 50, 50, 50, 50, 40, 40, 40, 30, 30)
+
+sort(Eggs)
+
+
+mean(Eggs)
+median(Eggs)
+mfv(Eggs)
+var(Eggs)
+sd(Eggs)
+quantile(Eggs, c(.25))
+quantile(Eggs, c(.50))
+quantile(Eggs, c(.75))
+
+
+
+
