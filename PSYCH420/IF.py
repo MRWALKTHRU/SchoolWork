bb925a07b4a7fbaca202ae7b10e08f50d50cb853
Jules Gravestock <114362927+MRWALKTHRU@users.noreply.github.com>
2025-02-28 14:57:57 -0500

    Update IF.py

--- PSYCH420/IF.py
+++ PSYCH420/IF.py
@@ -13,12 +13,12 @@ Program Instructions:
 """
 import numpy as np
 import matplotlib.pyplot as p
-
 # Define all the relevant variables
 initv = 0
 initi = 0
 initdv = 1
-vthresh = 0.25
+vthresh = 3
+maxv= 11
 c = 1
 r = 1
 tau = r * c
@@ -27,10 +27,6 @@ epochs = list(range(100))
 
 
 # Implement relevant equations
-# V(t + dt) = V(t) + dt/T(-(V-V(rest)) + R*I(t))
-# V(t) = -1(V(t + dt) + dt/T(-(V-V(rest)) + R*I(t)))
-# Tau(dV/dt) = -V(t) + R*I(t)
-
 
 def m_dvdt(i, v):
     return (-v + r * i) /tau
@@ -44,31 +40,48 @@ def m_i(v, dvdt):
     return v / r + c * dvdt
 
 
-voltage = [0]
+voltage = [initv]
 time = [0]
-current = [0]
+current = [initi]
 i = initi
 dvdt = initdv
+count = 100
+
+
 
 for _ in epochs:
-    i = 1 if 0.5 >= time[_] >= 0.25 else 0
-    newvoltage = (m_v(dvdt, voltage[-1], ts))
-    if voltage[-1] and voltage[_] == 0.5:
-        newvoltage = 0
-    dvdt = m_dvdt(i, voltage[-1])
-    voltage.append(newvoltage)
+    i = 10 if 3 >= time[_] >= 0.25 else 0 # Tells the current to be on for a short period
+    # Factor in refractory period
+    if count <= 5:
+        dvdt = m_dvdt(i/1000, voltage[-1])
+    elif count <= 15:
+        dvdt = m_dvdt(i/5, voltage[-1])
+    else:
+        dvdt = dvdt = m_dvdt(i, voltage[-1])
+        print(dvdt)
+
+    newvoltage = (m_v(dvdt, voltage[-1], ts)) # compute next voltage
+
+    # Code the voltage spike when threshold is exceeded
+    if voltage[_] >= vthresh and voltage[-1] != maxv:
+        voltage.append(maxv)
+        count = 0
+    elif voltage[-1] == maxv:
+        voltage.append(initv)
+        newvoltage = initv
+    else:
+        voltage.append(newvoltage)
+    count += 1
     time.append(time[-1]+ts)
     current.append(i)
-    if voltage[_] >= vthresh:
-        # Spike event
-        voltage[_] = 0.5
 
-        # Implement a refractory period?
-print(voltage)
-print(time)
-print(current)
 
-p.plot(time, voltage, 'b')
-p.plot(time, current, 'g')
+p.plot(time, voltage, 'black')
+p.plot(time, current, 'red')
+p.xlabel("Time (ms)")
+p.ylabel("Voltage (mV)")
+p.title("The Leaky Integrate-And-Fire Model of Neuron")
+p.legend(['Voltage', 'Current'], loc='upper right')
+
 p.show()
 
